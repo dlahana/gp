620a46915f1e4feb34343bf94fab5467a5e6c7dd
dlahana <dlahana@stanford.edu>
2021-11-09 14:39:16 -0800

    added write_geom method to terachem_io

--- terachem_io.py
+++ terachem_io.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import os
 
 def get_energy(path: str, method: str) -> float:
 
@@ -8,6 +9,7 @@ def get_energy(path: str, method: str) -> float:
     for line in lines:
         if ("FINAL ENERGY:" in line):
             energy = float(line.strip().split()[2])
+    f.close()
     
     return energy
 
@@ -25,12 +27,20 @@ def get_energy_gradient(n: int, path: str, method: str):
                 parsed = lines[index + 3 + i].strip().split()
                 for j in range(3):
                     data[3 * i + j + 1] = float(parsed[j])
-    
+    f.close()
+
     return data
 
 
-def write_geom(n: int, path:str, name: str):
+def write_geom(n: int, x, path:str, name: str):
 
+    f = open(os.path.join(path, name), "w")
+    f.write(str(n) + "\n\n")
+    for i in range(n):
+        f.write("%f %f %f\n", x[3 * i + 0], x[3 * i + 1], x[3 * i + 2])
+    f.close()
+
+    
     return
 
 
